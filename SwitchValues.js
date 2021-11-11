da3c2432941548886fbf950d9c135d6df0719251
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2021-11-11 16:23:55 -0300

    Arithmetic Operaters

--- SwitchValues.js
+++ SwitchValues.js
@@ -1,1 +1,15 @@
+/* Arithmetic Operators */
 let salary = 100;
+
+console.log(salary + salary);
+console.log(salary - salary);
+console.log(salary / salary);
+console.log(salary * salary);
+console.log(5 ** 2);
+
+/* ++ -- Increment and discrement */
+
+let age = 18;
+
+console.log(age--);
+console.log(age);
