34c9bfa498a1556ddee6076481623887bbe53b64
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2021-11-11 21:36:44 -0300

    Logical Operator

--- LogicalOperator.js
+++ LogicalOperator.js
@@ -1,10 +1,24 @@
-/* Logical Operator and (&&) */
+/* Logical Operator AND (&&) */
 /* Returns TRUE if both operands are true */
 
 console.log(true && true);
 
-let ofLegalAge = true;
+/* let ofLegalAge = true;
 let hasWorkCard = true;
 let canApply = ofLegalAge && hasWorkCard;
 
-console.log(canApply);
+console.log(canApply); */
+
+/* Logical Operator OR (||) */
+/* Returns TRUE if one of the operands are true */
+
+let ofLegalAge = false;
+let hasWorkCard = true;
+let canApply = ofLegalAge || hasWorkCard;
+
+console.log('Can apply: ', canApply);
+
+/* Logical Operator NOT (!) */
+let rejectedCandidate = !canApply;
+
+console.log('Rejected candidate: ', rejectedCandidate);
