9ce3a650f2182c889555d29f409d8087ad3a5d78
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2021-11-18 15:57:10 -0300

    Grade Average School

--- GradeAverageSchool.js
+++ GradeAverageSchool.js
@@ -11,5 +11,23 @@ const array = [70,70,80];
 console.log(studentAverage(array));
 
 function studentAverage(grades) {
+    const average = calculateAverage(grades)
+    if (average >= 0 && average <= 59)
+        return 'F';
+    if (average >= 60 && average <= 69)
+        return 'D';
+    if (average >= 70 && average <= 79)
+        return 'C';
+    if (average >= 80 && average <= 89)
+        return 'B';
     
+        return 'A';
+}
+
+function calculateAverage(array) {
+    let sum = 0;
+    for (let value of array) {
+        sum += value;
+    }
+    return sum/(array.length);
 }
