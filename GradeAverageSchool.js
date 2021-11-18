84af37026849e90ac6d22eef2eb2f4447eb26bd9
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2021-11-18 09:43:02 -0300

    Grade average school

---
+++ GradeAverageSchool.js
@@ -0,0 +1,15 @@
+/* Exercise grade school */
+/* Get the average from an array */
+
+/* 0-59: F */
+/* 60-69: D */
+/* 70-79: C */
+/* 80-89: B */
+/* 90-100: A */
+const array = [70,70,80];
+
+console.log(studentAverage(array));
+
+function studentAverage(grades) {
+    
+}
