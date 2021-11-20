c6d9f821444b54dc69d4a73d0efa96b31239b014
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2021-11-20 09:51:31 -0300

    Literal Template

--- LiteralTemplate.js
+++ LiteralTemplate.js
@@ -1,2 +1,25 @@
 /* Literal Template */
-const message = 'Hi this is \n my first message';
+const message = 
+'Hi this is \n' +
+' my \'first\' message';
+/* Object {} */
+/* Boolean true, false */
+/* String '',"" */
+/* Template `` */
+const other = `Hello this is my 
+'first' message`;
+
+const name = 'Phillip Stuart';
+const message2 = 'Hello' + ' ' + name + '\n';
+
+const email = 
+`${message2}
+
+Thanks for subscribing to my channel
+
+To follow the videos, don't forget to activate the bell
+
+Thank you,
+Lewis Armstrong`;
+
+console.log(email);
