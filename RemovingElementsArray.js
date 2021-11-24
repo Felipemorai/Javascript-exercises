55b094836ea16d456a7bba21505307a6edf3d6ab
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2021-11-24 20:32:36 -0300

    Removing Elements Array

---
+++ RemovingElementsArray.js
@@ -0,0 +1,18 @@
+/* Removing elements from array */
+const numbers = [1,2,3,4,5,6];
+numbers.push();
+numbers.unshift();
+numbers.splice();
+
+/* Final */
+const last = numbers.pop();
+console.log(last);
+console.log(numbers);
+/* First */
+const first = numbers.shift();
+console.log(first);
+console.log(numbers);
+/* Middle */
+const middle = numbers.splice(2,1);
+console.log(middle);
+console.log(numbers);
