4d5d27d013586ee2eb02a649ba4aedc794883cb3
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2021-11-24 16:25:39 -0300

    Find Element

---
+++ FindElement.js
@@ -0,0 +1,10 @@
+/* Find Elements (Primitive) */
+const number = [1,2,3,4];
+console.log(number.indexOf(2));
+
+const number2 = [1,2,3,1,4];
+console.log(number2.lastIndexOf(1));
+
+console.log(number.indexOf(2) !== -1);
+
+console.log(number.includes(2));
