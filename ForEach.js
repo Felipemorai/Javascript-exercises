5b672b5f6951f7f88fbad45c11645d92d63392b3
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2021-11-26 16:39:41 -0300

    ForEach

---
+++ ForEach.js
@@ -0,0 +1,10 @@
+/* Iterating an array */
+const numbers = [1,2,3,4,5];
+
+for (number of numbers)
+    console.log(number);
+
+/* ForEach */
+numbers.forEach((number) => {
+    console.log(number)
+});
